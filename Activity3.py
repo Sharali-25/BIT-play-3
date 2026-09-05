8d3c1e07a732fa5ab6f0a7ccda94daee079debeb
Sharali-25 <Sharaliarkansas@gmail.com>
2026-09-05 10:45:52 -0500

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,8 @@
+print("XOR sign detection = n ^ m < 0 means different signs ")
+print("4^2 = ",4^2,"same signs positive")
+print("4^-2 = ",4^-2,"different signs negative")
+n = int(input("Enter a number 5|-3"))
+print("XOR is negative when signs differ")
+print("",n,"^-8 = ",n^-8)
+
+print("Agar result negative hair toh dono numbers ke sign opposite hoge,else agar result positive hai to dono ke positive hoge.")
